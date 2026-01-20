48ac16ba2ae88238dd239435f195a2bdd5e40f32
Aferil <83399263+AferilVT@users.noreply.github.com>
2026-01-20 10:03:51 +0200

    Fixed issues

--- preload.js
+++ preload.js
@@ -1,9 +1,5 @@
-// Preload script for Neuro Karaoke Desktop
-// Currently minimal - can be extended if needed for future features
-
 const { contextBridge } = require('electron');
 
-// Expose any custom APIs to the renderer if needed in the future
 contextBridge.exposeInMainWorld('neuroKaraoke', {
   version: '1.0.0'
 });
